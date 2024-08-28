3a1ffc4fa9a62ae30fe84648c735a55cbbb7f55d
knarik1 <knarik.mh@gmail.com>
2024-08-28 19:36:55 +0400

    Integrate huggingface tokenizers.

--- test/datasets/test_checkpoint.py
+++ test/datasets/test_checkpoint.py
@@ -42,7 +42,7 @@ class TestCheckpoint:
         self, dataset_name, dataset_path, batch_size, seq_len, world_size, rank
     ):
         tokenizer_type = "tiktoken"
-        tokenizer = build_tokenizer("tiktoken", "./test/assets/test_tiktoken.model")
+        tokenizer = build_tokenizer("tiktoken", "./torchtitan/tokenizers/chemlactica-125m")
         return build_hf_data_loader(
             dataset_name=dataset_name,
             dataset_path=dataset_path,
--- torchtitan/tokenizers/tokenizer/__init__.py
+++ torchtitan/tokenizers/tokenizer/__init__.py
@@ -4,18 +4,12 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from torchtitan.tokenizers.tokenizer.sentencepiece import SentencePieceTokenizer
-from torchtitan.tokenizers.tokenizer.tiktoken import TikTokenizer
+from torchtitan.tokenizers.tokenizer.custom import CustomTokenizer
 from torchtitan.tokenizers.tokenizer.tokenizer import Tokenizer
-
 from torchtitan.logging import logger
 
 
 def build_tokenizer(tokenizer_type: str, tokenizer_path: str) -> Tokenizer:
-    logger.info(f"Building {tokenizer_type} tokenizer locally from {tokenizer_path}")
-    if tokenizer_type == "sentencepiece":
-        return SentencePieceTokenizer(tokenizer_path)
-    elif tokenizer_type == "tiktoken":
-        return TikTokenizer(tokenizer_path)
-    else:
-        raise ValueError(f"Unknown tokenizer type: {args.type}")
+    logger.info(f"Building tokenizer locally from {tokenizer_path}")
+
+    return CustomTokenizer(tokenizer_path)
