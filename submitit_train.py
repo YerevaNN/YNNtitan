a62643cd14c0ba951f66974834b84133553423c3
tigranfah <tigran.fahradyan01@gmail.com>
2024-10-02 10:59:09 +0400

    tune llama3.2 1b config

--- submitit_train.py
+++ submitit_train.py
@@ -6,7 +6,7 @@ import os
 
 if __name__ == "__main__":
     executor = submitit.AutoExecutor(folder="~/slurm_jobs/titan/job_%j")
-    n_gpus = 8
+    n_gpus = 4
     executor.update_parameters(
         name="titan", timeout_min=3 * 24 * 60,
         gpus_per_node=n_gpus,
@@ -19,9 +19,9 @@ if __name__ == "__main__":
     jobs = []
     with executor.batch():
         for _ in range(1):
-            train_config = './train_configs/chemlactica_125m.toml'
+            # train_config = './train_configs/chemlactica_125m.toml'
             # train_config = './train_configs/chemlactica_1.3b.toml'
-            # train_config = './train_configs/llama3_8b.toml'
+            train_config = './train_configs/llama3.2_1b.toml'
             # train_config = './train_configs/debug_model.toml'
             function = submitit.helpers.CommandFunction([
                 'python3', '-m', 'torch.distributed.run',
