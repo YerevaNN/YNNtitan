3d4946c6601898a8ce2cb3a3856c2ef84cef4647
tigranfah <tigran.fahradyan01@gmail.com>
2024-10-10 12:14:23 +0400

    add new learning rates, log the learning rate and track it on aim

--- train.py
+++ train.py
@@ -381,6 +381,7 @@ def main(job_config: JobConfig):
                     "train/loss_metrics/global_max_perplexity": global_max_perplexity,
                     "train/wps": wps,
                     "train/mfu(%)": mfu,
+                    "lr": lr_schedulers.last_lr,
                     "train/time_metrics/end_to_end(s)": time_end_to_end,
                     "train/time_metrics/data_loading(s)": time_data_loading,
                     "train/time_metrics/data_loading(%)": time_data_loading_pct,
@@ -400,7 +401,8 @@ def main(job_config: JobConfig):
                     f"{color.yellow}memory: {gpu_mem_stats.max_reserved_gib:5.2f}GiB"
                     f"({gpu_mem_stats.max_reserved_pct:.2f}%)  "
                     f"{color.blue}wps: {round(wps):,}  "
-                    f"{color.magenta}mfu: {mfu:.2f}%{color.reset}"
+                    f"{color.magenta}mfu: {mfu:.2f}%{color.reset}  "
+                    f"{color.red}lr: {lr_schedulers.last_lr:.3e}{color.reset}"
                 )
 
                 losses_since_last_log.clear()
