57656fd94fd64d30164f41e47ed3de8aae93d5ef
tigranfah <tigran.fahradyan01@gmail.com>
2024-10-19 11:51:25 +0400

    fix tokenizer path in llama 3.2 1b config

--- submitit_train.py
+++ submitit_train.py
@@ -1,19 +1,24 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
 import submitit
-import datetime
-import yaml
-import os
 
 
 if __name__ == "__main__":
     executor = submitit.AutoExecutor(folder="~/slurm_jobs/titan/job_%j")
     n_gpus = 8
+    node = "a100"
     executor.update_parameters(
-        name="titan", timeout_min=3 * 24 * 60,
+        name="titan",
+        timeout_min=6 * 24 * 60,
         gpus_per_node=n_gpus,
-        nodes=1, mem_gb=80, cpus_per_task=n_gpus * 4,
-        slurm_additional_parameters={
-            "partition": "h100"
-        }
+        nodes=1,
+        mem_gb=80,
+        cpus_per_task=n_gpus * 4,
+        slurm_additional_parameters={"partition": node},
     )
 
     jobs = []
@@ -21,19 +26,31 @@ if __name__ == "__main__":
         for _ in range(1):
             # train_config = './train_configs/chemlactica_125m.toml'
             # train_config = './train_configs/chemlactica_1.3b.toml'
-            train_config = './train_configs/llama3.2_1b.toml'
+            train_config = "./train_configs/llama3.2_1b.toml"
             # train_config = './train_configs/debug_model.toml'
-            function = submitit.helpers.CommandFunction([
-                'python3', '-m', 'torch.distributed.run',
-                '--nproc_per_node', f'{n_gpus}',
-                '--rdzv_backend', 'c10d',
-                '--rdzv_endpoint', 'localhost:0',
-                '--local-ranks-filter', '0',
-                '--role', 'rank', '--tee', '3',
-                'train.py',
-                '--job.config_file', train_config,
-            ])
-            print(' '.join(function.command))
+            function = submitit.helpers.CommandFunction(
+                [
+                    "python3",
+                    "-m",
+                    "torch.distributed.run",
+                    "--nproc_per_node",
+                    f"{n_gpus}",
+                    "--rdzv_backend",
+                    "c10d",
+                    "--rdzv_endpoint",
+                    "localhost:0",
+                    "--local-ranks-filter",
+                    "0",
+                    "--role",
+                    "rank",
+                    "--tee",
+                    "3",
+                    "train.py",
+                    "--job.config_file",
+                    train_config,
+                ]
+            )
+            print(" ".join(function.command))
             # subprocess.run(function.command)
             job = executor.submit(function)
             jobs.append(job)
