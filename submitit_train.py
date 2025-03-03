fd2475ceb1e4bfca5088aacfad8b572ee079438e
MenuaB <menooa.vold@gmail.com>
2025-03-03 09:55:08 +0400

    add changes for conformer training

--- submitit_train.py
+++ submitit_train.py
@@ -9,10 +9,15 @@ import submitit
 
 if __name__ == "__main__":
     executor = submitit.AutoExecutor(folder="~/slurm_jobs/titan/job_%j")
-    n_gpus = 6
     node = "h100"
+    n_gpus = 6
+    experiment_name = "3d_drugs_cart_2E_lr4e-3"
+    # executor = submitit.local.local.LocalExecutor(folder="~/slurm_jobs/titan/job_%j")
+    # node = "local"
+    # n_gpus = 1
+    # experiment_name = "conformer_conversion"
     executor.update_parameters(
-        name="titan",
+        name=experiment_name,
         timeout_min=24 * 24 * 60,
         gpus_per_node=n_gpus,
         nodes=1,
@@ -26,7 +31,9 @@ if __name__ == "__main__":
         for _ in range(1):
             # train_config = './train_configs/chemlactica_125m.toml'
             # train_config = './train_configs/chemlactica_1.3b.toml'
-            train_config = "./train_configs/llama3.2_1b.toml"
+            # train_config = "./train_configs/llama3.2_1b.toml"
+            train_config = "./train_configs/llama3.2_1b_conformers.toml"
+            # train_config = "./train_configs/llama3.2_1b_conversion.toml"
             # train_config = "./train_configs/llama3.2_3b.toml"
             # train_config = './train_configs/debug_model.toml'
             function = submitit.helpers.CommandFunction(
@@ -49,6 +56,12 @@ if __name__ == "__main__":
                     "train.py",
                     "--job.config_file",
                     train_config,
+                    "--metrics.aim_experiment_name",
+                    experiment_name,
+                    # "--training.num_decays",
+                    # "6",
+                    # "--training.decay_steps_perc",
+                    # "0.1",
                 ]
             )
             print(" ".join(function.command))
